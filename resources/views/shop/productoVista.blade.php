2b8139191fcd716349635193c090cef37ca13fc0
FherPie <pieandres@yahoo.com>
2020-01-01 15:20:36 -0500

    Se retira de la vista algunos elementos, Se los comenta

--- resources/views/shop/productoVista.blade.php
+++ resources/views/shop/productoVista.blade.php
@@ -33,7 +33,7 @@
                     </div>
                     <div class="details col-md-6">
                         <h3 class="product-title">{{$producto->nombre}}</h3>
-                        <div class="rating">
+          <!--               <div class="rating">
                             <div class="stars">
                                 <span class="fa fa-star checked"></span>
                                 <span class="fa fa-star checked"></span>
@@ -42,31 +42,31 @@
                                 <span class="fa fa-star"></span>
                             </div>
                             <span class="review-no">41 Revisiones</span>
-                        </div>
+                        </div> -->
                         <p class="product-description">{{$producto->descripcion}}</p>
                         <h4 class="price">Precio Actual: <span> {{
 				number_format($producto->precio_referencial_venta, 2, '.', ',')}}
 				USD</span></h4>
-                        <p class="vote"><strong>91%</strong> de los compradores les gustó este producto!! <strong>(87 Votos)</strong></p>
-                        <h5 class="sizes">Tamaños:
+                 <!--        <p class="vote"><strong>91%</strong> de los compradores les gustó este producto!! <strong>(87 Votos)</strong></p> -->
+                      <!--   <h5 class="sizes">Tamaños:
                             <span class="size" data-toggle="tooltip" title="small">s</span>
                             <span class="size" data-toggle="tooltip" title="medium">m</span>
                             <span class="size" data-toggle="tooltip" title="large">l</span>
                             <span class="size" data-toggle="tooltip" title="xtra large">xl</span>
-                        </h5>
-                        <h5 class="colors">Colores:
+                        </h5> -->
+             <!--            <h5 class="colors">Colores:
                             <span class="color orange not-available" data-toggle="tooltip" title="Not In store"></span>
                             <span class="color green"></span>
                             <span class="color blue"></span>
-                        </h5>
+                        </h5> -->
 						<h4 class="price">Mas Descripción Adicional: 
 						<p class="product-description">
 						{{$producto->informacion_adicional}}
 						</p>
 						</h4>
                         <div class="action">
-						<a href="{{route('producto.addToCart', ['id' => $producto->id_producto])}}" class=" add-to-cart btn btn-default pull-right">Agregar al carrito</a>
-                            <button class="like btn btn-default" type="button"><span class="fa fa-heart"></span></button>
+						<a href="{{route('producto.addToCart', ['id' => $producto->id_producto])}}" class=" add-to-cart btn btn-default pull-right">Agregar al carrito</a><!-- 
+                            <button class="like btn btn-default" type="button"><span class="fa fa-heart"></span></button> -->
                         </div>
                     </div>
                 </div>
